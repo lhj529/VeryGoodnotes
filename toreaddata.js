6864858d4c1374b5ef1aa6df182b3229c049ce64
lhj529 <2667621151@qq.com>
2026-01-10 22:00:40 +0800

    Add files via upload

--- toreaddata.js
+++ toreaddata.js
@@ -1,12 +1,13 @@
 var TOREAD_NOTES = {
   "tr_demo": {
-    "title": "Book title",
-    "author": "[country] author name",
-    "translator": "translator name",
+    "video": true,
+    "title": "Movie title",
+    "author": "[country] Author's name/Director’s name",
+    "translator": "translator's name",
     "press": "press name",
     "coverExt": "jpg", // 手动填写扩展名！如 "png", "webp"
-    "field": "书本类型/领域，例：政治小说",
-    "reason": "书本简介/想读原因",
+    "field": "类型/领域，例：喜剧电影",
+    "reason": "简介/想读原因/想看原因",
     "readers": {
       "猫": "reading",      
       "咩": "completed" // "todo"
@@ -19,5 +20,86 @@ var TOREAD_NOTES = {
       "猫":"5",
       "咩":"3.5"
     }
-  }}
+  },
+  "tr529": {
+   "video": false,
+    "title": "置身事内-中国政府与经济发展",
+    "author": "兰小欢",
+    "translator": "",
+    "press": "上海人民出版社",
+    "coverExt": "jpg",
+    "field": "政治,经济,社会",
+    "reason": "",
+    "readers": {
+      "猫":"todo",
+      "咩": "reading"
+    },
+    "complete_time": {
+      "咩": ""
+    },
+    "score": {
+      "咩": "5"
+    }
+  },
+  "tr530": {
+     "video": false,
+    "title": "人性的深渊-吴谢宇案",
+    "author": "吴琪、王珊",
+    "translator": "",
+    "press": "生活书店出版有限公司",
+    "coverExt": "png",
+    "field": "深度调查报道",
+    "reason": "",
+    "readers": {
+      "咩": "completed"
+    },
+    "complete_time": {
+      "咩": "2026-12-27"
+    },
+    "score": {
+      "咩": ""
+    }
+  },
+  "tr531": {
+     "video": false,
+    "title": "拉丁美洲被切开的血管",
+    "author": "[乌拉圭] 爱德华多·加莱亚诺",
+    "translator": "王玫 等",
+    "press": "南京大学出版社",
+    "coverExt": "jpg",
+    "field": "历史，社会，文学",
+    "reason": "",
+    "readers": {
+      "猫": "todo",
+      "咩": "todo"
+    },
+    "complete_time": {
+      "猫": "",
+      "咩": ""
+    },
+    "score": {
+      "猫": "",
+      "咩": ""
+    }
+  },
+  "tr532": {
+    "video": false,
+    "title": "唐诗的多维世界",
+    "author": "欧丽娟",
+    "translator": "",
+    "press": "北京大学出版社",
+    "coverExt": "jpg",
+    "field": "文学",
+    "reason": "",
+    "readers": {
+      "猫": "todo"
+    },
+    "complete_time": {
+      "猫": ""
+    },
+    "score": {
+      "猫": ""
+    }
+  },
+}
     

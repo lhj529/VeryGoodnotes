8d627c1272c4fe3fcb7732022cabf446476e6459
lhj529 <2667621151@qq.com>
2026-01-10 11:23:15 +0800

    Add files via upload

---
+++ toreaddata.js
@@ -0,0 +1,23 @@
+var TOREAD_NOTES = {
+  "tr_demo": {
+    "title": "Book title",
+    "author": "[country] author name",
+    "translator": "translator name",
+    "press": "press name",
+    "coverExt": "jpg", // 手动填写扩展名！如 "png", "webp"
+    "field": "书本类型/领域，例：政治小说",
+    "reason": "书本简介/想读原因",
+    "readers": {
+      "猫": "reading",      
+      "咩": "completed" // "todo"
+    },
+    "complete_time": {
+        "猫": "2025-10-10",      
+      "咩": "2025-12-12"
+    },
+    "score":{
+      "猫":"5",
+      "咩":"3.5"
+    }
+  }}
+    
